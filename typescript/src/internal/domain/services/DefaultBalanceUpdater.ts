0c8f91950ea114d4813a49e931b680e406664646
Lam Huy <lhuy0429@gmail.com>
2025-06-30 16:17:08 +0700

    fix: updateBalance logic

--- typescript/src/internal/domain/services/DefaultBalanceUpdater.ts
+++ typescript/src/internal/domain/services/DefaultBalanceUpdater.ts
@@ -4,9 +4,9 @@ import { IBalanceUpdater } from "../interfaces/IBalanceUpdater";
 class DefaultBalanceUpdater implements IBalanceUpdater {
     public updateBalance(timeDeposit: TimeDeposit, interest: number): void {
       const roundedInterest = Math.round((interest + Number.EPSILON) * 100) / 100;
-      timeDeposit.balance = Number(
-        (parseFloat(String(timeDeposit.balance) + parseFloat(String(roundedInterest))).toFixed(2))
-      );
+      timeDeposit.balance = 
+        parseFloat(String(timeDeposit.balance)) + parseFloat(String(roundedInterest))
+      ;
     }
   }
 
