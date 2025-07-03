77163ea3d136cb47b8b47d0b0c1d9191f7eabb9e
lamminhhuy <75303020+lamminhhuy@users.noreply.github.com>
2025-07-03 09:18:52 +0700

    fix: calculate bsic interest

--- typescript/src/internal/domain/strategies/BasicInterestStrategy.ts
+++ typescript/src/internal/domain/strategies/BasicInterestStrategy.ts
@@ -2,10 +2,7 @@ import { ICalculateInterestStrategy } from "../interfaces/ICalculateInterestStra
 
 class BasicInterestStrategy implements ICalculateInterestStrategy {
     calculateInterest(days: number,balance: number): number {
-        if (days < 366) {
             return (balance * 0.01) / 12;
-        }
-        return 0;
     }
 }
 
